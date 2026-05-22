2cb1eb03ad5aa33e5d14eadc60fecdcf8ebb6243
Yashika <yashika.kekulandala@inovaitsys.com>
2026-05-22 17:19:29 +0530

    I used postgreSQl as mt data base. Because it is it is an structural database we can use data in tables more user friendly. unique indexes are easy to unique column values. and  and easy to use and not interrupt when run with concurrent insertion, deletion ect.

---
+++ interview/src/main/java/org/example/interview/entity/Cache.java
@@ -0,0 +1,70 @@
+package org.example.interview.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "cache")
+public class Cache {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cache_key", nullable = false, unique = true)
+    private String key;
+
+    @Column(name = "cache_value")
+    private String value;
+
+    @Column(name = "last_accessed", nullable = false)
+    private Instant lastAccessed;
+
+    public Cache() {
+    }
+
+    public Cache(String key, String value, Instant lastAccessed) {
+        this.key = key;
+        this.value = value;
+        this.lastAccessed = lastAccessed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Instant getLastAccessed() {
+        return lastAccessed;
+    }
+
+    public void setLastAccessed(Instant lastAccessed) {
+        this.lastAccessed = lastAccessed;
+    }
+
+}
